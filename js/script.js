40f44bc410ff289b46dcef99747c0965629f5870
spigkaht <benj@betterrentals.com.au>
2024-02-15 21:41:16 +1100

    Added HTML content to frame out calculator UI
    Needs touching up
    
    CSS styles added to suit
    
    JavaScript functions and variables started

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const num1 = "";
+const num2 = "";
+const operator = "";
+
+const sum = () => {
+
+}
+
+const subtract = () => {
+
+}
+
+const multiply = () => {
+
+}
+
+const divide = () => {
+
+}
+
+const operate = (num1, num2, operate) => {
+  
+}
