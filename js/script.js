c846215f26f27b87ff850afd54955c0c8a8db191
spigkaht <benj@betterrentals.com.au>
2024-02-16 23:34:01 +1100

    JavaScript continued, HTML cleaned up to align with code
    Digit entry and clear button are functional
    
    Need to complete maths functions

--- js/script.js
+++ js/script.js
@@ -2,22 +2,47 @@ const num1 = "";
 const num2 = "";
 const operator = "";
 
-const sum = () => {
+const displayValue = 0;
 
+let count = 0;
+
+const clearButton = document.querySelector("#clear");
+const outputBox = document.querySelector("#output").childNodes[1];
+
+const clearOutputDisplay = () => {
+  outputBox.textContent = "";
 }
 
-const subtract = () => {
+const createOutputDisplay = () => {
+  const digitList = document.querySelectorAll(".digits");
 
-}
+  digitList.forEach((digit) => {
+    digit.addEventListener("click", (e) => {
+      if (outputBox.textContent === "0" && count === 0) {
+        outputBox.textContent = "";
+      }
 
-const multiply = () => {
+      if (outputBox.textContent.includes(".") && e.target.value === ".") {
+        return;
+      }
 
-}
+      outputBox.textContent += e.target.value;
 
-const divide = () => {
+      count++;
+    });
+  });
+};
 
-}
+const sum = () => {};
 
-const operate = (num1, num2, operate) => {
-  
-}
+const subtract = () => {};
+
+const multiply = () => {};
+
+const divide = () => {};
+
+const operate = (num1, num2, operate) => {};
+
+createOutputDisplay();
+
+clearButton.addEventListener("click", clearOutputDisplay);
